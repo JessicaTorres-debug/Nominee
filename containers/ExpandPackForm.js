1ec9a047f400b30144127a54c40f867f756a43cd
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-08-26 22:01:17 -0500

    Add input fields to the expand form

--- containers/ExpandPackForm.js
+++ containers/ExpandPackForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View, KeyboardAvoidingView } from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
 import MainHeader from '../components/MainHeader';
@@ -9,6 +9,7 @@ import SubHeader from '../components/SubHeader';
 const ExpandPackForm = ({ navigation }) => {
   return (
     <View style={styles.container}>
+    {/* <KeyboardAvoidingView style={styles.container} behavior="padding"> */}
       <StatusBar style="auto" />
 
       <View style={styles.topSection}>
@@ -16,11 +17,16 @@ const ExpandPackForm = ({ navigation }) => {
         <SubHeader title="Your Pack" />
       </View>
       <View style={styles.midSection}>
+        <TextInput style={styles.input} />
+        <TextInput style={styles.input} />
+        <TextInput style={styles.input} />
+        <TextInput style={styles.input} />
       </View>
       <View style={styles.bottomSection}>
         <PrimaryButton title="Add" />
         <PrimaryButton title="Finished" onPress={() => navigation.navigate('Share And Start')} />
       </View>
+    {/* </KeyboardAvoidingView> */}
     </View>
   );
 }
@@ -33,14 +39,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#A3B18A',
   },
   topSection: {
-    flex: 3,
+    // flex: 2,
     top: 0,
   },
   midSection: {
-    flex: 2,
+    // flex: 2,
   },
   bottomSection: {
-    flex: 1,
+    // flex: .5,
+  },
+  input: {
+    width: 280,
+    height: 43,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#707070',
+    borderStyle: 'solid',
   },
 });
 
